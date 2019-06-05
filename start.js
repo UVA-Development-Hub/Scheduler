59dc808cf5f034b3563ede24da73d01fe7d6073c
gmf5de <gmf5de@virginia.edu>
2019-06-05 11:49:28 -0400

    add heroku port support

--- start.js
+++ start.js
@@ -1,6 +1,13 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-}).listen(8080);
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+
+app.listen(port);
