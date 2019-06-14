5813d840492319bd33c0810ec52bf4e0bf2e14a8
Sauhil Faraz <sauhilfaraz@d-172-25-246-57.dhcp.virginia.edu>
2019-06-14 15:43:02 -0400

    changes

--- public/js/jQuery.js
+++ public/js/jQuery.js
@@ -1,5 +1,11 @@
 var currentPage = 0;
+var numberOfPages = 0;
 $(document).ready( function() {
+  numberOfPages = parseInt($("#max_page_div").attr("max_page"));
+  for(var i = 0; i < numberOfPages; i++) {
+      $("#button_box").append("<button onclick=displayPage(" + i + ")>" + (i + 1) + "</button>");
+  };
+  console.log($("#button_box"));
   displayPage(0);
 
 });
@@ -7,7 +13,7 @@ $(document).ready( function() {
 
 function displayPage(pageNumber){
   $('.paged').each(function(){
-      if(parseInt($(this).attr('page')) == pageNumber) { // attr('page') gets data from the class 'page'
+      if(parseInt($(this).attr('page')) ==  pageNumber) { // attr('page') gets data from the class 'page'
           console.log("page 0");
           $(this).show();
       } else {
