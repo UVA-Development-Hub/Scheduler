d02c9d88ec7b664eb63f3914ffb3178ccacb4ada
Sierra <sierra@d-172-25-72-217.dhcp.virginia.edu>
2019-06-21 15:57:24 -0400

    page swaping

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,25 +1,38 @@
 var currentPage = 0;
 var numberOfPages = 0;
+var arr;
 $(document).ready( function() {
   numberOfPages = parseInt($("#max_page_div").attr("max_page"));
-  for(var i = 0; i < numberOfPages; i++) {
-      $("#button_box").append("<button onclick=displayPage(" + i + ")>" + (i + 1) + "</button>");
-  };
-  console.log($("#button_box"));
-  displayPage(0);
+  //for(var i = 0; i < numberOfPages; i++) {
+      //$("#button_box").append("<button onclick=displayPage(" + i + ")>" + (i + 1) + "</button>");
+  //};
+  //console.log($("#button_box"));
+  arr= $(".pageselector");
+  displayPage(5);
 
 });
-
 
 function displayPage(pageNumber){
   $('.paged').each(function(){
       if(parseInt($(this).attr('page')) ==  pageNumber) { // attr('page') gets data from the class 'page'
-          console.log("page 0");
+          //console.log("page 0");
           $(this).show();
       } else {
         $(this).hide();
       }
     })
+
+    for(var i=0; i<5;i++ ){
+      $(arr[i]).attr("onclick", "displayPage("+ parseInt(pageNumber-5+i)+")");
+      $(arr[i]).html(parseInt(pageNumber-5+i));
+      console.log("test statment");
+    }
+
+    for(var i=5; i<10;i++ ){
+      $(arr[i]).attr("onclick", "displayPage("+ parseInt(pageNumber+(i-5))+")");
+      $(arr[i]).html(parseInt(pageNumber+(i-5)));
+    }
+
 };
 
 
