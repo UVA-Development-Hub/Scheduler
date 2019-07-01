c93deb487c8d1d348ae6142acb9593ceb331f115
Sauhil Faraz <sauhilfaraz@d-172-25-246-57.dhcp.virginia.edu>
2019-07-01 14:02:03 -0400

    changes to course.js, trying to determine the course ID as numerical or mnemonic

--- routes/course.js
+++ routes/course.js
@@ -9,22 +9,39 @@ router.get('/:term/:id', (req, res) => {
     // A mnemonic id consists of the subject and class number together: "CS2150", "STS1500", etc
     // A numberical id consists of just a number, like 10253
     // mongo.getTerms('', {subject: req.params.subject}, {catalog_number: req.params.catalog_number} )
-    mongo.getTerms('', {sis_id: req.params.id}, )
-    res.send("Display information on class with catalog (or mnemonic) id " + req.params.id + " from term " + req.params.term + ".");
+    // mongo.getTerms('', {sis_id: req.params.id}, )
+    if (isNaN(req.params.id)) {
+        split(req.params.id)
+    }
+    else {
+        mongo.searchTerm(req.params.term, {req.params.id}, data) => {
+            console.log(data);
+            res.render('course/term/id', {
+                id: req.params.id
+            });
+        }
+    }
+
+    // if it's a number then you have to search for it
+    // if it's nemonic you have to break it apart
+    // res.send("Display information on class with catalog (or mnemonic) id " + req.params.id + " from term " + req.params.term + ".");
     // res.send(req.params.subject + req.params.catalog_number);
 });
 
 router.get('/:term', (req, res) => {
-    mongo.searchTerm('1192', {}, data => {
+    mongo.searchTerm(req.params.term, {}, data => {
        console.log(data);
        res.render('course/term', {
            title: 'Classes in term ' + req.params.term,
-           courses: data
+           courses: data,
+           // subject: data.subject
        });
    });
 
+
     res.send("Display all classes from term " + req.params.term + ".");
 });
+
 
 router.get('/', (req, res) => {
     res.send("The future home of a page which explains the course view, searching for courses, etc.");
