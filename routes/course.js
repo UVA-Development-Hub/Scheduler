1f7ac6c60774b66152d690961f02c4696b7333f9
Sauhil Faraz <sauhilfaraz@d-172-25-246-57.dhcp.virginia.edu>
2019-07-01 12:21:44 -0400

    test push

--- routes/course.js
+++ routes/course.js
@@ -8,8 +8,10 @@ router.get('/:term/:id', (req, res) => {
     // In this view, you need to determine whether the id given was a mnemonic id or a numerical id.
     // A mnemonic id consists of the subject and class number together: "CS2150", "STS1500", etc
     // A numberical id consists of just a number, like 10253
+    // mongo.getTerms('', {subject: req.params.subject}, {catalog_number: req.params.catalog_number} )
     mongo.getTerms('', {sis_id: req.params.id}, )
     res.send("Display information on class with catalog (or mnemonic) id " + req.params.id + " from term " + req.params.term + ".");
+    // res.send(req.params.subject + req.params.catalog_number);
 });
 
 router.get('/:term', (req, res) => {
