91cdb67e68ee4f63a0685910f62b8db5bae8eac1
G.Michael Fitzgerald <gmf5de@virginia.edu>
2019-07-14 16:22:26 -0400

    Fetch search results and terms list in parallel

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,11 @@
 var express = require('express'),
     router = express.Router(),
     appdir = require('path').dirname(require.main.filename),
-    mongo=require("../bin/mongo.js");
-const fetch = require("node-fetch");
+    mongo = require("../bin/mongo.js"),
+    async = require('async');
 
 router.get('/login', function (req, res) {
-    if(req.session.user) res.redirect('/profile');
+    if(req.session.user) res.redirect('/');
     else {
         req.query.return = '/profile';
         res.render('login', {
@@ -73,73 +73,75 @@ router.get('/search', function(req, res){
 
 router.post('/search', function(req, res){
     var thedays = [
-       req.body.monday,
-       req.body.tuesday,
-       req.body.wednesday,
-       req.body.thursday,
-       req.body.friday,
-       req.body.saturday,
-    ],
-        daynames = [
-           'M',
-           'T',
-           'W',
-           'R',
-           'F',
-           'S'
-    ],
-        dayinput = '',
+        req.body.monday,
+        req.body.tuesday,
+        req.body.wednesday,
+        req.body.thursday,
+        req.body.friday,
+        req.body.saturday,
+    ], daynames = [
+        'M',
+        'T',
+        'W',
+        'R',
+        'F',
+        'S'
+    ], dayinput = '',
         tosubmit = {};
 
     thedays.forEach((checkbox, index) => {
         if(checkbox) dayinput = dayinput + daynames[index];
     });
 
+    if (req.body.catalog_number !='') tosubmit.catalog_number =req.body.catalog_number;
     if(req.body.subject != '') tosubmit.subject = req.body.subject.toUpperCase();
     if(dayinput != '') tosubmit.days = dayinput;
-    console.log(tosubmit);
-    if (req.body.catalog_number !='') tosubmit.catalog_number =req.body.catalog_number;
 
     function tocompare(courseList,course){
-        var ret=-1;
-        for(i=0;i<courseList.length;i++){
-            if(course.subject === courseList[i].subject && course.catalog_number === courseList[i].number){
-                ret=i;
+        var ret = -1;
+        for(var i = 0; i < courseList.length; i++) {
+            if(course.subject === courseList[i].subject && course.catalog_number === courseList[i].number) {
+                ret = i;
                 break;
             }
         }
         return ret;
     }
 
-    mongo.getTerms(termsList => {
-        //console.log('term: ' + req.body.term_id);
-        mongo.searchTerm(req.body.term_id, tosubmit, (err, results) => {
-            var new_result = [],
+    // Retrieve the search results and terms list in parallel
+    async.parallel([
+        async.reflect( callback => {
+            mongo.getTerms(callback);
+        }),
+        async.reflect( callback => {
+            mongo.searchTerm(req.body.term_id, tosubmit, callback);
+        })
+    ], (err, data) => {
+        var new_result = [],
             itemIndex = 0;
-            for (x = 0; x < results.length; x++){
-                itemIndex = tocompare(new_result,results[x]);
-                if(itemIndex > -1) new_result[itemIndex].section.push(results[x]);
-                else {
-                    new_result.push({
-                        subject:results[x].subject,
-                        number: results[x].catalog_number,
-                        title: results[x].title,
-                        section: [
-                            results[x]
-                        ]
-                    });
-                }
+        for (x = 0; x < data[1]['value'].length; x++) {
+            itemIndex = tocompare(new_result, data[1]['value'][x]);
+            if(itemIndex > -1) new_result[itemIndex].section.push(data[1]['value'][x]);
+            else {
+                new_result.push({
+                    subject:data[1]['value'][x].subject,
+                    number: data[1]['value'][x].catalog_number,
+                    title: data[1]['value'][x].title,
+                    section: [
+                        data[1]['value'][x]
+                    ],
+                });
             }
+        }
 
-            res.render('search', {
-                title : 'Search Page',
-                user: req.session.passport.user,
-                terms: termsList,
-                results: new_result,
-                input: req.body,
-                selected_term: req.body.term_id,
-                user: req.session.user
-            });
+        res.render('search', {
+            title : 'Search Page',
+            user: req.session.passport.user,
+            terms: data[0],
+            results: new_result,
+            input: req.body,
+            selected_term: req.body.term_id,
+            user: req.session.user
         });
     });
 });
