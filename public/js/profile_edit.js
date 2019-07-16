39a8ca5cca465abc082e18d4f437c6844b79d271
gmf5de <gmf5de@virginia.edu>
2019-07-16 13:39:04 -0400

    Implement better behavior of the double major checkbox in profile edit

--- public/js/profile_edit.js
+++ public/js/profile_edit.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $('#double_major').change(function() {
         $('#double_major_input').toggle('slow');
     });
+
+    if($('#double_major').is(':checked')) {
+        $('#double_major_input').show();
+    }
 });
