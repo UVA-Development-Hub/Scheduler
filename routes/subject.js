aaddd62b9e9a6f6f0bae5946bc7d30a55add2021
Sauhil Faraz <sauhilfaraz@d-172-25-246-57.dhcp.virginia.edu>
2019-07-16 14:57:05 -0400

    subject.js is now working, can obtain the courses for a subject from the most recent term. subject page is in pogress for styling

--- routes/subject.js
+++ routes/subject.js
@@ -3,23 +3,6 @@ var router = express.Router();
 var mongo = require('../bin/mongo.js');
 
 
-
-// router.get('/:subject', (req, res) => {
-//     mongo.getTerms(termsList => {
-//         res.render('subject/subject', {
-//             terms: Array.from(termsList),
-//             recentTerm: terms[terms.length - 1]
-//         });
-//         mongo.searchTerm(recentTerm, {'subject': req.params.subject}, (err, data) => {
-//             console.log(data);
-//             res.render('subject/subject', {
-//                 course_subjects: data['subject']
-//             });
-//         });
-//     });
-//
-// });
-
 router.get('/:subject', (req, res) => {
     mongo.getTerms((err,termsList) => {
         //console.log("Terms:\n",termsList);
