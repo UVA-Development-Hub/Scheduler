ea9e76bb3530d1b597fd48bdef75b65f32d6745b
Jason Ashley <jasona99@outlook.com>
2019-07-17 13:59:19 -0400

    Ported code from search for subject. Consider making module?

--- routes/subject.js
+++ routes/subject.js
@@ -5,12 +5,39 @@ var mongo = require('../bin/mongo.js');
 
 router.get('/:subject', (req, res) => {
     mongo.getTerms((err,termsList) => {
+        function tocompare(courseList,course){
+            var ret = -1;
+            for(var i = 0; i < courseList.length; i++) {
+                if(course.subject === courseList[i].subject && course.catalog_number === courseList[i].number) {
+                    ret = i;
+                    break;
+                }
+            }
+            return ret;
+        }
         //console.log("Terms:\n",termsList);
         var recentTerm = termsList[termsList.length-1]["_id"];
         mongo.searchTerm(recentTerm, {'subject': req.params.subject}, (err, data) => {
+            //console.log(data);
             console.log(data);
+            var new_result = [],
+                itemIndex = 0;
+            for (x = 0; x < data.length; x++) {
+                itemIndex = tocompare(new_result, data[x]);
+                if(itemIndex > -1) new_result[itemIndex].section.push(data[x]);
+                else {
+                    new_result.push({
+                        subject:data[x].subject,
+                        number: data[x].catalog_number,
+                        title: data[x].title,
+                        section: [
+                            data[x]
+                        ],
+                    });
+                }
+            }
             res.render('subject/subject', {
-                course_subjects: data,
+                course_subjects: new_result,
             });
         });
     });
