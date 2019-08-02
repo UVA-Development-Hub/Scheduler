b0289ed8070f08c7369821c1b1a5feae12d5e139
Jason Ashley <jasona99@outlook.com>
2019-08-02 04:01:15 -0400

    Proper support for adding dictionary to search with.

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -4,11 +4,20 @@ var arr;
 $(document).ready( function() {
     arr= $(".pageselector");
     d = getUrlParams(location.search);
+    var pgInfo = {};
+    var maxResults = 25;
+    if ($('#subject').text()){
+        pgInfo.subject = $('#subject').text();
+        if ($('#catalog_number').text()){
+            pgInfo.catalog_number = $('#catalog_number').text();
+            maxResults = Number.MAX_SAFE_INTEGER;
+        }
+    }
     currentPage = parseInt(d.page || 1) - 1;
     if(currentPage < 0) currentPage = 0;
     window.history.pushState({"html": $('html')[0].outerHTML, "pageTitle": $("title").text()},"", window.location.href.replace(/([&?]page=)(.*)(&{1}.*){0,1}/, '$1' + (currentPage + 1) + '$3'));
     currentPage--;
-    loadPage(currentPage + 1);
+    loadPage(currentPage + 1, pgInfo, maxResults);
 });
 
 function setButtons(pageNumber) {
@@ -53,8 +62,8 @@ function setButtons(pageNumber) {
 
 };
 
-function loadPage(pg) {
-    getSearchPage(parseInt(pg), 25, (data, max, term) => {
+function loadPage(pg, pgInfo, maxResults) {
+    getSearchPage(parseInt(pg), maxResults, (data, max, term) => {
         maxPage = max - 1;
         // Write new search results to the #results element
         var newHTML = ` `, i = 0;
@@ -64,7 +73,7 @@ function loadPage(pg) {
             var courseLink = "/course/"+term+"/"+course.subject+currentCatalog
             const subjectHeader = `
                 <div class="row">
-                    <div class="col-md-12" style="margin-top:50px;">
+                    <div class="col-md-12" style="margin-top:15px;">
                         <h3 class="bg-uva-orange course-header">
                             <a href=${courseLink}>
                                 <b>${course.subject} ${currentCatalog} - ${course.title}</b>
@@ -147,7 +156,7 @@ function loadPage(pg) {
         // Load the new html into the frame
         $("#results").html(newHTML);
         setButtons(pg);
-    });
+    }, pgInfo);
 }
 
 function urlAndLoad(pg) {
